3f537f5f947e882513b48808046da33e46e525cc
DiDream <alexjimmy1993@hotmail.com>
2017-06-01 23:24:45 +0100

    Pruebas para Plato, Menu, Dieta

--- spec/dietaryMenu_spec.rb
+++ spec/dietaryMenu_spec.rb
@@ -14,10 +14,84 @@ RSpec.describe DLL do
     end
     it "Correcta inserción de valores" do
         arr =[1,2,3,4,5]
-        i = arr.length-1
+        i = 0
         @dll.each do |node|
             expect(node.value).to be(arr[i])
-            i-=1
+            i+=1
         end
     end
+end
+RSpec.describe Plato do
+    before :all do
+        @plato = Plato.new({
+            descripcion: 'Macarrones con salsa de tomate y queso parmesano',
+            porcion: '1 1/2 cucharon',
+            gramos: 200
+            })
+    end
+    it "Debe existir la descripcion de un plato" do
+        expect(@plato).to respond_to(:descripcion)
+    end
+    it "Debe existir la porcion de un plato" do
+        expect(@plato).to respond_to(:porcion)
+    end
+    it "Debe existir la ingesta de gramos" do
+        expect(@plato).to respond_to(:gramos)
+    end
+end
+RSpec.describe Menu do
+    before :all do
+        platos = DLL.new(
+            Plato.new({
+                descripcion: 'Macarrones con salsa de tomate y queso parmesano',
+                porcion: '1 1/2 cucharon',
+                gramos: 200
+            }),
+            Plato.new({
+                descripcion: 'Escalope de ternera',
+                porcion: '1 bistec mediano',
+                gramos: 100
+            }),
+            Plato.new({
+                descripcion: 'Ensalada basica con zanahoria rallada',
+                porcion: 'guarnicion de 120 g',
+                gramos: 120
+            }),
+            Plato.new({
+                descripcion: 'Mandarina',
+                porcion: '1 grande',
+                gramos: 180
+            }),
+            Plato.new({
+                descripcion: 'Pan de trigo integral',
+                porcion: '1 rodaja',
+                gramos: 20
+            })
+
+        )
+        @menu = Menu.new({
+            titulo: 'Almuerzo',
+            porcentaje_diario: '30 - 35%',
+            platos: platos,
+            vct: 785.9,
+            proteinas: 19,
+            grasas: 34,
+            hidratos: 47
+            })
+    end
+    it "Debe existir un conjunto de platos" do
+        expect(@menu).to be_instance_of(Menu)
+    end
+    it "Obtener un plato del menu" do
+        expect(@menu[0]).to be_instance_of(Plato)
+    end
+end
+RSpec.describe Dieta do
+    before :all do
+        @dieta = Dieta.new(3200, 4)
+    end
+    it "La dieta tiene un valor de ingesta diaria recomendada" do
+        expect(@dieta).to respond_to(:ingesta_diaria)
+    end
+
 end
