f5ab4213c8c29d1116a17a55c5c0f37ee4d5534a
DiDream <alexjimmy1993@hotmail.com>
2017-06-01 20:55:41 +0100

    Pruebas para DLL

--- spec/dietaryMenu_spec.rb
+++ spec/dietaryMenu_spec.rb
@@ -4,8 +4,20 @@ RSpec.describe DietaryMenu do
   it "has a version number" do
     expect(DietaryMenu::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+end
+RSpec.describe DLL do
+    before :all do
+        @dll = DLL.new(1,2,3,4,5)
+    end
+    it "Es una lista doblemente enlazada. Sus nodos tienen :next y :prev" do
+        expect(@dll.head).to respond_to(:next, :prev)
+    end
+    it "Correcta inserción de valores" do
+        arr =[1,2,3,4,5]
+        i = arr.length-1
+        @dll.each do |node|
+            expect(node.value).to be(arr[i])
+            i-=1
+        end
+    end
 end
